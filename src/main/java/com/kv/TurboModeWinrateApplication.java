5c1fd151300067ce974c77b2bfb176314ac6815d
Kaivalya Thanekar <kaivalyathanekar@gmail.com>
2023-01-13 02:35:57 +0530

    Add Spring Cloud Config support. Removed Swagger due to Spring Boot 2.6 and up in-compatibility

--- src/main/java/com/kv/TurboModeWinrateApplication.java
+++ src/main/java/com/kv/TurboModeWinrateApplication.java
@@ -8,11 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -22,21 +17,21 @@ import java.util.List;
 
 @SpringBootApplication
 @Configuration
-@EnableSwagger2
+//@EnableSwagger2
 public class TurboModeWinrateApplication {
 
     public static void main(String[] args) throws URISyntaxException, IOException {
         SpringApplication.run(TurboModeWinrateApplication.class, args);
     }
 
-    @Bean
-    public Docket getDocket() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.kv"))
-                .paths(PathSelectors.any())
-                .build();
-    }
+//    @Bean
+//    public Docket getDocket() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.kv"))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
 
     @Bean
     public RestTemplate getRestTemplate() {
